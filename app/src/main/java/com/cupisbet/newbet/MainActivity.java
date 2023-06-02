9c00254c825d994659d7f350bf1ef3174f33b10a
LoboBOB12 <63793995+LoboBOB12@users.noreply.github.com>
2023-06-02 22:47:56 +0300

    1

--- app/src/main/java/com/cupisbet/newbet/MainActivity.java
+++ app/src/main/java/com/cupisbet/newbet/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
-        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setSelectedItemId(R.id.match);
 
     }
 
